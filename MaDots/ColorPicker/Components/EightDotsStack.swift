8a6e7c72545d06e8bc4af382a332606e4b04ec05
Lorenzo Fortes <mrlorenzo1608@gmail.com>
2025-05-16 14:22:35 -0300

    Alteracoes branch

---
+++ MaDots/ColorPicker/Components/EightDotsStack.swift
@@ -0,0 +1,20 @@
+//
+//  EightDotsStack.swift
+//  MaDots
+//
+//  Created by Lorenzo Fortes on 15/05/25.
+//
+
+import UIKit
+
+class EightDotsStack: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
