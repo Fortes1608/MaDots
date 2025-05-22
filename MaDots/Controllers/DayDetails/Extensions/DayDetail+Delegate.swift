760542c2e074b9d7886214af95307b4a36b85fa1
AnaPoletto <ana.palhares@edu.pucrs.br>
2025-05-22 17:32:30 -0300

    Ultimo Commit

---
+++ MaDots/Controllers/DayDetails/Extensions/DayDetail+Delegate.swift
@@ -0,0 +1,13 @@
+//
+//  DayDetail+Delegate.swift
+//  MaDots
+//
+//  Created by Ana Carolina Palhares Poletto on 22/05/25.
+//
+
+import UIKit
+
+extension DayDetailViewController: UICollectionViewDelegate {
+    
+    
+}
